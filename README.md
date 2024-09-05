a6ac90fb33cf3c565725c2601dc6e6a0d78715bc
flatFeather <github.astronaut143@silomails.com>
2024-09-05 03:05:54 +0200

    Update readme

--- README.md
+++ README.md
@@ -1,31 +1,26 @@
 # t3-hashviz-dart
-
-A Dart package for generating hash visualization (commonly referred to as Identicons) based on provided hash. It is one of the Greatwall TKBA protocol implementations. It returns different types of visualization for given hashes.
-This visualization generation relies on [Blockies](https://pub.dev/packages/blockies)
+A Dart package for generating hash visualizations (commonly referred to as Identicons) based on a provided hash. It is one of the Greatwall TKBA protocol implementations. This package generates the data for the visualization and delegates the final rendering with colors to the client.
 
 ## Features
-
-- Generate hash visualization (commonly referred to as Identicons) based on hash.
-- Customize the size, color, background color, and spotColor.
-  
+- Generate hash visualizations (commonly referred to as Identicons) based on a hash.
+- Customize the size of the blocks in the visualization.
 
 ## Getting started
-
 To start using Hashviz, add it to your `pubspec.yaml`:
 ```yaml
 dependencies:
-    hashviz: ^1.0.0
+  hashviz: ^1.0.0
 ```
+
 Then, run `flutter pub get` to install the package.
 
-### Running tests
-#### Unit test
-Unit tests have to be executed using the flutter framework since Blockies extends a flutter component. Therefore the command 'flutter test' is used.
+## Running tests
+Unit tests can be executed using the Dart framework. Run the following command:
 ```bash
-flutter test
+dart test
 ```
-#### Example
-In order to visualize the behavior of the module, an example with flutter has been developed. To run these test, the following commands must be executed:
+## Example
+To visualize the behavior of the module, an example with Flutter has been developed. To run these tests, execute the following commands:
 ```bash
 cd example
 code .
@@ -33,23 +28,22 @@ flutter run
 ```
 
 ## Usage
+Example code on how to use Hashviz to generate hash visualizations (commonly referred to as Identicons) pattern:
 
-Example code on how to use Hashviz to generate hash visualization (commonly referred to as Identicons) pattern.
 ```dart
 import 'package:hashviz/hashviz.dart';
 
 void main() {
   // Create a Hashviz instance
-  Hashviz hashviz = Hashviz();
+  Hashviz hashviz = Hashviz(size: 8); // Initialize with the desired size
 
-  // Generate pattern based on a hash string
+  // Generate pattern data based on a hash string
   String hash = 'your_hash_string';
-  Blockies pattern = hashviz.generatePattern(hash);
+  List<int> patternData = hashviz.generatePatternData(hash);
+
+  // Render the pattern data with colors in your Flutter app
 }
 ```
 
 ## Additional information
-
-TODO: Tell users more about the package: where to find more information, how to
-contribute to the package, how to file issues, what response they can expect
-from the package authors, and more.
+TODO: Tell users more about the package: where to find more information, how to contribute to the package, how to file issues, what response they can expect from the package authors, and more.
--- test/hashviz_test.dart
+++ test/hashviz_test.dart
@@ -1,123 +1,33 @@
+import 'package:test/test.dart';
+
 import 'package:hashviz/hashviz.dart';
 
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
-
-import 'dart:ui';
-
 void main() {
-  group('Hashviz tests', () {
-    test('Constructor without parameters', () {
-      final hashviz = Hashviz(size: 8);
-      expect(hashviz.size, 8);
-      expect(hashviz.backgroundColor, isNull);
-      expect(hashviz.color, isNull);
-      expect(hashviz.spotColor, isNull);
+  group('Hashviz Tests', () {
+    test('Constructor should throw ArgumentError for non-positive size', () {
+      expect(() => Hashviz(size: 0), throwsArgumentError);
+      expect(() => Hashviz(size: -1), throwsArgumentError);
+      expect(() => Hashviz(size: 1), returnsNormally);
     });
 
-    test('Constructor with all parameters', () {
-      final hashviz = Hashviz(
-        size: 8,
-        backgroundColor: 0xFFFFFFFF,
-        color: 0xFF000000,
-        spotColor: 0xFF00FFFF,
-      );
-      expect(hashviz.size, 8);
-      expect(hashviz.backgroundColor, 0xFFFFFFFF);
-      expect(hashviz.color, 0xFF000000);
-      expect(hashviz.spotColor, 0xFF00FFFF);
+    test('Generate pattern data should produce non-empty list', () {
+      final hashviz = Hashviz(size: 16);
+      final hash = 'test_hash';
+      final imageData = hashviz.generatePatternData(hash);
+
+      expect(imageData, isNotEmpty);
+      expect(imageData, hasLength(16 * 16)); // For size 16, expect 256 values
     });
 
-    test('Constructor throws ArgumentError when invalid size', () {
-      expect(
-        () => Hashviz(size: -5),
-        throwsArgumentError,
-      );
-    });
+    test('CreateRandSeed should generate consistent seed', () {
+      final hashviz = Hashviz(size: 16);
+      final hash1 = 'seed1';
+      final hash2 = 'seed2';
 
-    test('Constructor throws ArgumentError when invalid background color', () {
-      expect(
-        () => Hashviz(size: 8, backgroundColor: 0x100000000),
-        throwsArgumentError,
-      );
-    });
+      final seed1 = hashviz.createRandSeed(hash1);
+      final seed2 = hashviz.createRandSeed(hash2);
 
-    test('Constructor throws ArgumentError when invalid color', () {
-      expect(
-        () => Hashviz(size: 8, color: 0x100000000),
-        throwsArgumentError,
-      );
-    });
-
-    test('Constructor throws ArgumentError when invalid spot color', () {
-      expect(
-        () => Hashviz(size: 8, spotColor: 0x100000000),
-        throwsArgumentError,
-      );
-    });
-
-    test('generatePattern method from hashviz with required attributes', () {
-      final hashviz = Hashviz(size: 8);
-
-      final blockies = hashviz.generatePattern('example_hash');
-
-      expect(blockies.seed, 'example_hash');
-      expect(blockies.size, 8);
-      expect(blockies.color, isNull);
-      expect(blockies.bgColor, isNull);
-      expect(blockies.spotColor, isNull);
-    });
-
-    test('generatePattern method from hashviz with optional attributes', () {
-      final hashviz = Hashviz(
-        size: 8,
-        backgroundColor: 0xFFFFFFFF,
-        color: 0xFF000000,
-        spotColor: 0xFF00FFFF,
-      );
-
-      final blockies = hashviz.generatePattern('example_hash');
-
-      expect(blockies.seed, 'example_hash');
-      expect(blockies.size, 8);
-      expect(blockies.color, Color(0xFF000000));
-      expect(blockies.bgColor, Color(0xFFFFFFFF));
-      expect(blockies.spotColor, Color(0xFF00FFFF));
-    });
-
-    test('Getter and setters when no errors', () {
-      final hashviz = Hashviz(size: 10);
-      hashviz.backgroundColor = 0xFF000000;
-      hashviz.color = 0xFFFFFFFF;
-      hashviz.spotColor = 0xFF00FFFF;
-      expect(hashviz.size, 10);
-      expect(hashviz.backgroundColor, 0xFF000000);
-      expect(hashviz.color, 0xFFFFFFFF);
-      expect(hashviz.spotColor, 0xFF00FFFF);
-    });
-
-    test('Setter for backgroundColor throws ArgumentError when invalid color', () {
-      final hashviz = Hashviz(size: 8);
-      expect(
-        () => hashviz.backgroundColor = 0x100000000, // Invalid color
-        throwsArgumentError,
-      );
-    });
-
-    test('Setter for color throws ArgumentError when invalid color', () {
-      final hashviz = Hashviz(size: 8);
-      expect(
-        () => hashviz.color = 0x100000000, // Invalid color
-        throwsArgumentError,
-      );
-    });
-
-    test('Setter for spot color throws ArgumentError when invalid color', () {
-      final hashviz = Hashviz(size: 8);
-      expect(
-        () => hashviz.spotColor = 0x100000000, // Invalid color
-        throwsArgumentError,
-      );
+      expect(seed1, isNot(equals(seed2)));
     });
   });
 }
