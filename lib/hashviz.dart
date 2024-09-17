5a686014902b67749a53cba948f244ebc069a85e
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2024-09-17 12:47:55 +0300

    Fix library meta-data and README file.

--- lib/hashviz.dart
+++ lib/hashviz.dart
@@ -1,7 +1,7 @@
 // TODO: Complete the copyright.
 // Copyright (c) 2024, ...
 
-/// A library for generating a visualization from hash (commonly known as Identicons).
+/// A library for generating a visualization blocks from a hash (commonly known as Identicons).
 library;
 
 export 'src/hashviz_base.dart';
